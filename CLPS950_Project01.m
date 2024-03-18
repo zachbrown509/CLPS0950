0ae78b0b6eea50c1252a0cee6df3c5f1024374bb
sydnutakor <90426333+sydnutakor@users.noreply.github.com>
2024-03-18 16:46:12 -0400

    Add files via upload

--- CLPS950_Project01.m
+++ CLPS950_Project01.m
@@ -4,7 +4,7 @@ disp('Welcome to our Personality Quiz: What Coffee are You?')
 disp('You will answer the following ten questions and rank yourself 1-10 based on how much you agree with the statement.')
 
 % Initialize the total score
-total_score = 0;
+total_score = zeros(1,length(questions));
 
 % Questions
 questions = {
@@ -14,12 +14,12 @@ questions = {
     'How much do you enjoy nature? (1-10)';
     };
 
-beverages = {
+coffee = {
 'Vanilla Iced Coffee'; 'Hot Brewed Coffee'; 'Cold Brew';
 'Iced Lavender Latte'; 'Espresso Macchiato';    
 };
 
-beverage_recipes = {
+coffee_recipes = {
 'Vanilla Iced Coffee Recipe: ',
 'Hot Brewed Coffee Recipe: ',
 'Cold Brew Recipe: ',
@@ -30,5 +30,11 @@ beverage_recipes = {
 for ii = 1:length(questions)
     total_score(ii) = input(questions{ii});
 end
-disp(sum(total_score));
+disp(sum(total_score)); %% delete this later
+
+[~,coffee_index] = max(total_score);
+chosen_coffee = coffee{coffee_index};
+chosen_recipe = coffee_recipes{coffee_index};
+
+disp(chosen_coffee);
 
