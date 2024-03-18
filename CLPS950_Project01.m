f688953252675bddd2973f0a626b11b446532909
zachbrown509 <163910171+zachbrown509@users.noreply.github.com>
2024-03-18 16:14:16 -0400

    Add files via upload

--- CLPS950_Project01.m
+++ CLPS950_Project01.m
@@ -1,4 +1,19 @@
-%%%%% Questions %%%%%
+%%%%% CLPS0950 Project 1: Personality Quiz %%%%%
 
 disp('Welcome to our Personality Quiz: What Coffee are You?')
 disp('You will answer the following ten questions and rank yourself 1-10 based on how much you agree with the statement.')
+
+% Initialize the total score
+total_score = 0;
+
+% Questions
+questions = {
+    'How adventerous are you on a scale of 1-10?';
+    'Rate how much of a morning person you are on a scale of 1-10';
+    'How extroverted are you? (1-10)';
+    'How much do you enjoy nature? (1-10)';
+    };
+for ii = 1:length(questions)
+    disp(questions{ii})
+end
+
