afd9f201ead91719c0bcf3e4628f791a336b8950
zachbrown509 <163910171+zachbrown509@users.noreply.github.com>
2024-03-18 15:36:13 -0400

    Add files via upload

---
+++ CLPS950_Project01.m
@@ -0,0 +1,4 @@
+%%%%% Questions %%%%%
+
+disp('Welcome to our Personality Quiz: What Coffee are You?')
+disp('You will answer the following ten questions and rank yourself 1-10 based on how much you agree with the statement.')
